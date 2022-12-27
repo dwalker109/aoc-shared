b9ab44e671f2afc39b9ed7dda8b2e0593ed11740
Dan Walker <dwalker109@gmail.com>
2022-12-27 02:04:56 +0000

    Replace timings crate with simple Instant usage

--- src/runner.rs
+++ src/runner.rs
@@ -1,20 +1,16 @@
-use std::fmt::Display;
+use std::{fmt::Display, time::Instant};
 
-use logging_timer::timer;
-
-/// Runs and emits both parts of an AOC day via provided closures, with timings (if RUST_LOG=debug).
+/// Runs and emits both parts of an AOC day via provided closures, with timings.
 pub fn solve<T: Display, F1: FnOnce() -> T, F2: FnOnce() -> T>(f1: F1, f2: F2) {
-    env_logger::init();
+    let (r, d) = {
+        let _tmr = Instant::now();
+        (f1(), Instant::now().duration_since(_tmr))
+    };
+    println!("Part 1: {r} ({:?})", d);
 
-    let r1 = {
-        let _tmr = timer!("Part 1");
-        f1()
+    let (r, d) = {
+        let _tmr = Instant::now();
+        (f2(), Instant::now().duration_since(_tmr))
     };
-    println!("Part 1: {r1}");
-    
-    let r2 = {
-        let _tmr = timer!("Part 2");
-        f2()
-    };
-    println!("Part 2: {r2}");
+    println!("Part 2: {r} ({:?})", d);
 }
